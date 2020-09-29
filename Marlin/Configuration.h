f8a760d522292bd5940e5c5e70a1df19cfcd8244
santi19z <santi.zambrana19@gmail.com>
2020-09-29 14:23:34 -0600

    PID Changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,9 +493,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.0 }
     #define DEFAULT_Kd_LIST { 114.00, 112.0 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  15.61
+    #define DEFAULT_Ki   1.26
+    #define DEFAULT_Kd  48.49
   #endif
 #endif // PIDTEMP
 
